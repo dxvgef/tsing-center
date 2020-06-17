ed54819e9593e7d0d7e2c76e476eb56182f74ae5
dxvgef <dxvgef@outlook.com>
2020-06-17 14:51:23 +0800

    更新注释

--- global/var.go
+++ global/var.go
@@ -12,5 +12,5 @@ var (
 	Storage StorageType // 存储器实例
 
 	Services sync.Map // 所有服务列表,key=serviceID, value=loadBalance
-	Nodes    sync.Map // 所有节点列表,key=serviceID, value={load_balance.InstType}
+	Nodes    sync.Map // 所有节点列表,key=serviceID, value=load_balance.Build()
 )
